022679eaef4dcc49b06fc83634d667e1b7e368bf
gogolashvili <ggogolashvili91@gmail.com>
2023-02-18 15:41:47 +0400

    add j s  function

---
+++ main.js
@@ -0,0 +1,6 @@
+let searchinput = document.querySelector("input");
+let searchicon = document.querySelector("img");
+
+searchicon.addEventListener("click", function () {
+  searchinput.classList.toggle("toggle");
+});
